238e9d8dd468be6e7a3f1fd71d590acc17d29c08
mark-starikov <supermark2005@mail.ru>
2023-10-14 16:28:52 +0700

    создал стили для блога и скрипт для открытия

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const navbar = document.querySelector(".navbar");
 const mMenuToggle = document.querySelector(".navbar-menu-toggle");
 const menu = document.querySelector(".navbar-mobile");
 const modalDialog = document.querySelector(".modal.dialog");
+let shoeBlogBtn = document.querySelector(".shoe-blog-button");
+let blog = document.querySelector(".blog");
 const openMenu = (event) => {
   // Стрелочная функция открывания меню
   menu.classList.add("is-open"); // Добавляем класс к меню
@@ -14,6 +16,25 @@ const closeMenu = (event) => {
   mMenuToggle.classList.remove("close-menu"); // Удаляем класс с кнопки меню
   document.body.style.overflow = ""; // Страница двигается
 };
+const openBlogMenu = () => {
+  // Стрелочная функция открывания меню
+  blog.classList.add("is-open"); // Добавляем класс к меню
+  navbar.classList.add("is-open"); // Добавляем класс к меню
+  shoeBlogBtn.classList.add("close-menu"); // Добавляем класс к кнопке меню
+  document.body.style.overflow = "hidden"; // Когда меню открыто, страница не двигается
+};
+const closeBlogMenu = () => {
+  // Функция закрытия меню
+  blog.classList.remove("is-open"); // Удаляем класс с меню
+  navbar.classList.remove("is-open"); // Удаляем класс с меню
+  shoeBlogBtn.classList.remove("close-menu"); // Удаляем класс с кнопки меню
+  document.body.style.overflow = ""; // Страница двигается
+};
+shoeBlogBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  blog.classList.contains("is-open") ? closeBlogMenu() : openBlogMenu();
+});
+
 mMenuToggle.addEventListener("click", (event) => {
   // Функция при клике на кнопку меню
   event.preventDefault(); // Отключаем переход по ссылке для кнопки меню
