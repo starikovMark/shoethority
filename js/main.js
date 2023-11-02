b3845af8b2223da6092baca0c84c6efc17de1141
starikovMark <supermark2005@mail.ru>
2023-11-02 12:11:35 +0700

    добавил: мобильное меню, кнопки навигации

--- js/main.js
+++ js/main.js
@@ -6,16 +6,20 @@ let shoeBlogBtn = document.querySelector(".shoe-blog-button");
 let blog = document.querySelector(".blog");
 let searchBtn = document.querySelector(".search-button");
 let search = document.querySelector(".search");
+let shoeList = document.querySelector(".shoe-list");
+let shoeMobileBtn = document.querySelector(".shoe");
 
 const openMenu = (event) => {
   // Стрелочная функция открывания меню
   menu.classList.add("is-open"); // Добавляем класс к меню
+  navbar.classList.add("is-open"); // Добавляем класс к меню
   mMenuToggle.classList.add("close-menu"); // Добавляем класс к кнопке меню
   document.body.style.overflow = "hidden"; // Когда меню открыто, страница не двигается
 };
 const closeMenu = (event) => {
   // Функция закрытия меню
   menu.classList.remove("is-open"); // Удаляем класс с меню
+  navbar.classList.remove("is-open"); // Добавляем класс к меню
   mMenuToggle.classList.remove("close-menu"); // Удаляем класс с кнопки меню
   document.body.style.overflow = ""; // Страница двигается
 };
@@ -62,3 +66,8 @@ mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault(); // Отключаем переход по ссылке для кнопки меню
   menu.classList.contains("is-open") ? closeMenu() : openMenu(); // если меню содержит класс is-open, выполняется функция закрытия меню, в противном случае меню открывается.
 });
+shoeMobileBtn.addEventListener("click", () => {
+  shoeList.classList.toggle("is-open");
+});
+console.log(shoeMobileBtn);
+console.log(shoeList);
